b325fa27e47d70952916e239bb0a61ccffb7b721
Eugene Arutyunov <e@intuition.team>
2020-09-29 20:46:10 +0300

    wip

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,6 +3,9 @@ import $ from "jquery";
 
 const ready = function() {
   let currentColorScheme = "";
+  let currentGlobalUnits = "";
+  let currentPxInEm = "";
+  let unitsListner;
 
   if (localStorage.getItem("color-scheme")) {
     let colorScheme = localStorage.getItem("color-scheme");
@@ -16,9 +19,41 @@ const ready = function() {
     ).val();
   }
 
+  if (localStorage.getItem("global-units")) {
+    let globalUnits = localStorage.getItem("global-units");
+    setGlobalUnits(globalUnits);
+    $(`.units-selector input:radio`)
+      .filter(`[value=${globalUnits}]`)
+      .prop("checked", true);
+  } else {
+    currentColorScheme = $(
+      ".units-selector input:radio[name=units-selector]:checked"
+    ).val();
+  }
+
+  if (localStorage.getItem("px-in-em")) {
+    let pxInEm = localStorage.getItem("px-in-em");
+    setPxValues(pxInEm);
+    $(".em-converter input").val(pxInEm);
+  } else {
+    currentPxInEm = $(".em-converter input").val();
+  }
+
   $(".js-color-switch input:radio").change(function() {
     setColorScheme($(this).val());
   });
+
+  $(".units-selector input:radio").change(function() {
+    setGlobalUnits($(this).val());
+  });
+
+  unitsListner = setInterval(function() {
+    let pxInEm = $(".em-converter input").val();
+
+    if (pxInEm != currentPxInEm) {
+      setPxValues(pxInEm);
+    }
+  }, 300);
 
   function setColorScheme(newColorScheme) {
     $("body").removeClass(currentColorScheme);
@@ -26,6 +61,56 @@ const ready = function() {
     localStorage.setItem("color-scheme", newColorScheme);
     currentColorScheme = newColorScheme;
   }
+
+  function setGlobalUnits(newGlobalUnits) {
+    switch (newGlobalUnits) {
+      case "em":
+        $(".em-converter").removeClass("visible");
+
+        $(".px").hide();
+        $(".em").show();
+        break;
+      case "px":
+        $(".em-converter").addClass("visible");
+
+        $(".px").show();
+        $(".em").hide();
+    }
+
+    localStorage.setItem("global-units", newGlobalUnits);
+    currentGlobalUnits = newGlobalUnits;
+  }
+
+  function setPxValues(newPxInEm) {
+    $(".value").each(function() {
+      let emValue = $(this)
+        .find(".em")
+        .text();
+      let pxValue = Math.floor(emValue * newPxInEm * 10) / 10;
+      $(this)
+        .find(".px")
+        .text(pxValue);
+    });
+
+    localStorage.setItem("px-in-em", newPxInEm);
+    currentPxInEm = newPxInEm;
+  }
+
+  $(".js-scroll-link").click(function(e) {
+    e.preventDefault();
+
+    var linkOffset = -40;
+    if ($($.attr(this, "href")).data("scroll-link-offset")) {
+      linkOffset += $($.attr(this, "href")).data("scroll-link-offset");
+    }
+
+    $("html, body").animate(
+      {
+        scrollTop: $($.attr(this, "href")).offset().top + linkOffset
+      },
+      500
+    );
+  });
 };
 
 $(document).ready(ready);
